0e87e716ef5ed924446063aabf120a25cb7e5063
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-10-30 13:34:34 -0400

    update buttons, linux support

--- bare_bones_mp.py
+++ bare_bones_mp.py
@@ -1,17 +1,19 @@
 """
-    Bare Bones YouTube Player Demo with Playlist
+    Bare Bones Media Player with Playlist
     Author      :   Israel Dryer
     Modified    :   2019-10-30
 """
-import PySimpleGUI as sg 
 import vlc
+import PySimpleGUI as sg 
+from sys import platform as PLATFORM
+
 sg.change_look_and_feel('DarkBlue')
 
 # media player gui
 def btn(name):
     return sg. Button(name, size=(6, 1), pad=(1, 1), key=name.upper())
 
-layout = [[sg.Input(default_text='Video URL or Local Path:', size=(30, 1), key='NEW'), sg.Submit(key='SUBMIT')],
+layout = [[sg.Input(default_text='Video URL or Local Path:', size=(30, 1), key='NEW'), btn('load')],
           [sg.Image('', size=(300, 170), key='VID_OUT')],
           [btn('previous'), btn('play'), btn('next'), btn('pause'), btn('stop')],
           [sg.Text('Load media to start', key='TIME')]]
@@ -24,7 +26,10 @@ list_player = inst.media_list_player_new()
 media_list = inst.media_list_new([])
 list_player.set_media_list(media_list)
 player = list_player.get_media_player()
-player.set_hwnd(window['VID_OUT'].Widget.winfo_id())
+if PLATFORM.startswith('linus'):
+    player.set_xwindow(window['VID_OUT'].Widget.winfo_id)
+else:
+    player.set_hwnd(window['VID_OUT'].Widget.winfo_id())
 
 # main event loop
 while True:
@@ -45,9 +50,9 @@ while True:
         list_player.previous() 
         list_player.previous() # 2 calls needed to get to prior??
         list_player.play()
-    if event == 'SUBMIT':
+    if event == 'LOAD':
         vid = values['NEW']
-        if not 'Video URL' in vid: 
+        if not 'Video URL' in vid and vid != '': 
             media_list.add_media(vid)
             list_player.set_media_list(media_list)
             window['NEW'].update(value = 'Video URL or Local Path:') # only add a legit submit
